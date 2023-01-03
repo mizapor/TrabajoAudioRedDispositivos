c9a76734fccc7049779ab8e667a91ec4bd7b67b8
Zzemog <kikogomezalemany@gmail.com>
2023-01-03 22:04:03 +0100

    Webcam Wall added

---
+++ TrabajoAudioRedDispositivos/Assets/Scripts/Webcam.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Webcam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
