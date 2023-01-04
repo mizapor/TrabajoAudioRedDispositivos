122d7a8030fd743b226518713d4ad4fdc095e2a1
PabloSF <62628314+pabletefest@users.noreply.github.com>
2023-01-04 19:28:55 +0100

    Added basic multiplayer MIRROR

---
+++ TrabajoAudioRedDispositivos_clone_1/Assets/HitDetection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDetection : MonoBehaviour
+{
+    public GameObject spherePrefab;
+    public Transform[] sphereInitPositions;
+
+    //private bool canHitSphere = true;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Sphere"/* && canHitSphere*/)
+        {
+            //canHitSphere = false;
+            Debug.Log("Collided with :" + other.tag);
+            Instantiate(spherePrefab, sphereInitPositions[Random.Range(0, sphereInitPositions.Length)].position, Quaternion.identity);
+        }
+    }
+
+    //void OnTriggerExit(Collider other)
+    //{
+    //    canHitSphere = true;
+    //}
+}
